67d09442aee2478f780ee77c1bf7ccc9f00f5389
yaacovk10 <yaacovana@gmail.com>
2024-01-31 16:25:44 +0200

    implement Bootstrap Navigation in navigation side bar

--- src/features/counter/Menu.tsx
+++ src/features/counter/Menu.tsx
@@ -24,16 +24,20 @@ export function Menu() {
       <div className="flex-grow-1 p-3">
         {selectedLessonId && <LessonContent lessonId={selectedLessonId} />}
       </div>
-      <div className="flex-shrink-0 p-3 bg-light" style={{width: "280px"}}>
-        <ul className="nav nav-pills flex-column mb-auto">
+      <div className="flex-shrink-0 p-3 bg-light" style={{ width: "280px" }}>
+        <Nav defaultActiveKey="/home" className="flex-column">
           {items.map(item => (
-            <li className="nav-item" key={item.id}>
-              <a href="#" className={`nav-link ${selectedLessonId === item.id ? 'active' : ''}`} onClick={() => handleMenuItemClick(item.id)}>
-                {item.name_hebrew}
-              </a>
-            </li>
+            <Nav.Link
+              key={item.id}
+              eventKey={item.id.toString()}
+              href="#"
+              onClick={() => handleMenuItemClick(item.id)}
+              active={selectedLessonId === item.id}
+            >
+              {item.name_hebrew}
+            </Nav.Link>
           ))}
-        </ul>
+        </Nav>
       </div>
     </div>
   );
