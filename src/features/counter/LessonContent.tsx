8c9b1c08490929537b43f13403c8e27c9352a310
yaacovk10 <yaacovana@gmail.com>
2024-02-05 12:26:06 +0200

    add a photo for each word

--- src/features/counter/LessonContent.tsx
+++ src/features/counter/LessonContent.tsx
@@ -1,17 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { AppDispatch, RootState } from '../../app/store';
-import { fetchContent } from './LessonContentSlice';
+import { LessonContentItem, fetchContent } from './LessonContentSlice';
 
+
+const apiKey =  'o5Ck3cUb2KGWhBS6JGemEySVYjdWsquLK0NtrFvGD1YdyxagNM1bBD1G';
+
+interface Photos {
+    [key: string]: string;
+  }
+  
 const LessonContent = ({ lessonId }: { lessonId: number }) => {
     const dispatch = useDispatch<AppDispatch>();
     const { content, status, error } = useSelector((state: RootState) => state.lessonContent);
+    const [photos, setPhotos] = useState<Photos>({});
+    
     console.log("lessonId: " + lessonId)
+    
     useEffect(() => {
         dispatch(fetchContent(lessonId));
     }, [dispatch, lessonId])
 
-
+    useEffect(() => {
+        content.forEach((word: LessonContentItem) => {
+          fetch(`https://api.pexels.com/v1/search?query=${word.word_english}&per_page=1`, {
+            headers: {
+              'Authorization': apiKey,
+            }
+          })
+          .then(response => response.json())
+          .then(data => {
+            if (data.photos.length > 0) {
+              setPhotos(prev => ({ ...prev, [word.id]: data.photos[0].src.large }));
+            }
+          })
+          .catch(console.error);
+        });
+      }, [content]);
 
     // Render content based on status
     if (status === 'loading') return <div>Loading...</div>;
@@ -22,6 +47,7 @@ const LessonContent = ({ lessonId }: { lessonId: number }) => {
             <p>English: {item.word_english}</p>
             <p>Hebrew: {item.word_hebrew}</p>
             <p>French: {item.word_french}</p>
+            {photos[item.id] && <img src={photos[item.id]} alt={item.word_english}/>}
         </div>
     ))}
     </div>;
